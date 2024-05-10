fdb4e9297eb1587a8fa94d96ec477b06eec2acba
Stevie Hryciw <codroid@gmail.com>
2024-05-09 21:32:20 -0700

    Have AppRun exec redis server and CLI

--- AppRun/main.go
+++ AppRun/main.go
@@ -4,12 +4,27 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"path/filepath"
+	"time"
 )
 
 func main() {
-	cmd := exec.Command("./subprogram/subprogram")
+	self, err := os.Executable()
+	if err != nil { panic(err) }
+	base := filepath.Dir(self)
+
+	cmd := exec.Command(base + "/bin/redis-server")
 	cmd.Stdout = os.Stdout
-	if err := cmd.Run(); err != nil {
+	if err := cmd.Start(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer cmd.Wait()
+
+	time.Sleep(time.Second)
+	cmd2 := exec.Command(base + "/bin/redis-cli", "set", "hello", "goodbye")
+	cmd2.Stdout = os.Stdout
+	if err := cmd2.Run(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
